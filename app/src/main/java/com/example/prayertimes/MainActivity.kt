2c644a7a2a5e956a0c6bd7a5dab42588bea248f6
namr97002-boop <namr97002@gmail.com>
2026-08-25 03:29:38 +0300

    Refactor MainActivity to use WebView directly
    Refactored MainActivity to use a local WebView instance and removed unused alarm scheduling methods.

--- app/src/main/java/com/example/prayertimes/MainActivity.kt
+++ app/src/main/java/com/example/prayertimes/MainActivity.kt
@@ -1,134 +1,23 @@
 package com.example.prayertimes
 
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var webView: WebView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        val webView = WebView(this)
 
-        webView = findViewById(R.id.webView)
-
-        // إعداد WebView
-        webView.settings.apply {
-            javaScriptEnabled = true
-            domStorageEnabled = true
-            databaseEnabled = true
-            allowFileAccess = true
-            allowContentAccess = true
-        }
-
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
         webView.webViewClient = WebViewClient()
 
-        // ربط Android مع JavaScript
-        webView.addJavascriptInterface(this, "Android")
-
-        // تحميل التطبيق
         webView.loadUrl("https://namr97002-boop.github.io/Al-Noor/")
-    }
 
-    @JavascriptInterface
-    fun schedulePrayer(
-        name: String,
-        label: String,
-        timeInMillis: Long
-    ) {
-        try {
-
-            val alarmManager =
-                getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-            val intent = Intent(this, AdhanReceiver::class.java).apply {
-                putExtra("PRAYER_NAME", label)
-            }
-
-            val pendingIntent = PendingIntent.getBroadcast(
-                this,
-                name.hashCode(),
-                intent,
-                PendingIntent.FLAG_UPDATE_CURRENT or
-                        PendingIntent.FLAG_IMMUTABLE
-            )
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
-                alarmManager.setExactAndAllowWhileIdle(
-                    AlarmManager.RTC_WAKEUP,
-                    timeInMillis,
-                    pendingIntent
-                )
-
-            } else {
-
-                alarmManager.setExact(
-                    AlarmManager.RTC_WAKEUP,
-                    timeInMillis,
-                    pendingIntent
-                )
-            }
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    @JavascriptInterface
-    fun cancelAllPrayers() {
-
-        try {
-
-            val alarmManager =
-                getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-            val prayers = listOf(
-                "Fajr",
-                "Dhuhr",
-                "Asr",
-                "Maghrib",
-                "Isha"
-            )
-
-            for (prayer in prayers) {
-
-                val intent =
-                    Intent(this, AdhanReceiver::class.java)
-
-                val pendingIntent =
-                    PendingIntent.getBroadcast(
-                        this,
-                        prayer.hashCode(),
-                        intent,
-                        PendingIntent.FLAG_UPDATE_CURRENT or
-                                PendingIntent.FLAG_IMMUTABLE
-                    )
-
-                alarmManager.cancel(pendingIntent)
-            }
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    override fun onBackPressed() {
-
-        if (webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
-        }
+        setContentView(webView)
     }
 }
